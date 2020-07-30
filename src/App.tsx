45e35261f908ab0104e20a8cd0ea4aac97c271d8
Ingo Andelhofs <ingoandelhofs@gmail.com>
2020-07-30 19:59:24 +0200

    Updated some bit components and added a component for handling the current theme.

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import {BrowserRouter as Router, Route} from "react-router-dom";
 
 // Example code imports
 import "./examples/css/global.scss";
-import "./css/main.scss";
 import BubbleTheme from "./examples/BubbleTheme";
 import DefaultTheme from "./examples/DefaultTheme";
+import DefaultStyle from "./components/styles/DefaultStyle";
 
 
 function App() {
   return <Router>
     {/* Example routes */}
+    <DefaultStyle />
+
     <Route path="/" exact component={DefaultTheme}/>
     <Route path="/bubble" exact component={BubbleTheme}/>
   </Router>;
---
+++ src/components/styles/DefaultStyle.tsx
@@ -0,0 +1,8 @@
+import React, {FC} from "react";
+import "../../css/main.scss";
+
+const DefaultStyle: FC<any> = () => {
+  return <></>;
+}
+
+export default DefaultStyle;
