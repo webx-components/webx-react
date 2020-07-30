11757fd3282e8d405fd85b555f33b03b706780c2
Ingo Andelhofs <ingoandelhofs@gmail.com>
2020-07-30 15:38:14 +0200

    Refactored code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 
-import "./styles/global.scss";
-
-import BubbleTheme from "./themes/BubbleTheme";
-import DefaultTheme from "./themes/DefaultTheme";
+// Example code imports
+import "./examples/css/global.scss";
+import "./css/bubble/forms.scss";
+import BubbleTheme from "./examples/BubbleTheme";
+import DefaultTheme from "./examples/DefaultTheme";
 
 
 function App() {
   return <Router>
+    {/* Example routes */}
     <Route path="/" exact component={DefaultTheme}/>
     <Route path="/bubble" exact component={BubbleTheme}/>
   </Router>;
